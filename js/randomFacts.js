6d0827babd66aebe62f83fa6824ab690ef5843da
dfSabogal10 <david.sabogal1@gmail.com>
2017-02-15 10:35:57 -0500

    modificaciones

--- js/randomFacts.js
+++ js/randomFacts.js
@@ -1,27 +1,39 @@
-var facts=[];
-facts.push("I like soccer");
-facts.push("I'am a Real Madrid Fan");
-facts.push("I'am a Millonarios FC Fan");
-facts.push("I have a Motorcycle");
-facts.push("I like to go to the gym");
-facts.push("I'am from Bogota");
-facts.push("I like Reggae music");
-facts.push("I love playing FIFA");
-facts.push("My High School is in Mosquera");
-facts.push("My favourite Netflix Show is Daredevil");
-facts.push("I used to Skate");
-facts.push("I've never been out of the country");
+var facts = [];
+
+function Fact(f,img){
+this.f=f;
+this.image=img;
+}
+
+facts.push(new Fact("I like soccer","soccer.jpg"));
+facts.push(new Fact("I'am a Real Madrid Fan","realmadrid.jpg"));
+facts.push(new Fact("I'am a Millonarios FC Fan","millonarios.jpg"));
+facts.push(new Fact("I have a Motorcycle","motorcycle.jpg"));
+facts.push(new Fact("I like to go to the gym","gym.jpg"));
+facts.push(new Fact("I'am from Bogota","bogota.jpg"));
+facts.push(new Fact("I like Reggae music","reggae.jpg"));
+facts.push(new Fact("I love playing FIFA","fifa.jpg"));
+facts.push(new Fact("My High School is in Mosquera","highschool.jpg"));
+facts.push(new Fact("My favourite Netflix Show is Daredevil","daredevil.jpg"));
+facts.push(new Fact("I used to Skate","skate.jpg"));
+facts.push(new Fact("I've never been out of the country","foreign.jpg"));
+
+
+
 function showFact(){
 var i;
 var text;
 var fact;
 i = (Math.floor(Math.random()*11));
 fact= facts[i];
-text = document.createTextNode(fact);
+text = document.createTextNode(fact.f);
 var parrafo = document.getElementById("fact");
+var seccion = document.getElementById("factimage");
 while (parrafo.firstChild) {
     parrafo.removeChild(parrafo.firstChild);
 }
 parrafo.appendChild(text);
-};
-showFact();
+seccion.setAttribute("src","img/"+fact.image);
+
+}
+
